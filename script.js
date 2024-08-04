6acd28972714e33a786b26b5256e5ec0f75f6c01
shaik1014 <shaikizaaz00@gmail.com>
2024-08-04 13:21:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    document.getElementById('logo').style.display='none'
+    document.getElementById('loader-text').style.display='none'
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+})
